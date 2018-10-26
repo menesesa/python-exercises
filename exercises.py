7205d5e27904542076dc9876d41fab8916e2b10b
Alejandro <ameneses91@gmail.com>
2018-10-26 14:43:15 +0200

    create linear search function

---
+++ exercises.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 26 14:07:41 2018
+
+@author: alemeneses
+"""
+
+#%%
+
+def linear(lst, number): 
+    for i in range(len(lst)):
+        if lst[i] == number: 
+            return i 
+        
+        return None
+    
+    
